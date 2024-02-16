9907030ce0fbb0cf4b54381cc68ba0b77db11370
STD KING <112249837+StdKing@users.noreply.github.com>
2024-02-16 21:54:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,7 +67,7 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ALLTYPECC")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FEELING_MOTIVATED")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DAXXSUPPORT")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
