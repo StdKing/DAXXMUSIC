6b9a9c0949a337e924f187c77281ef1bf2a2fc2e
STD KING <112249837+StdKing@users.noreply.github.com>
2024-03-04 15:38:13 +0530

    Update nexio.py

--- DAXXMUSIC/plugins/sudo/nexio.py
+++ DAXXMUSIC/plugins/sudo/nexio.py
@@ -53,7 +53,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["akur","hakur"], prefixes=["t", "T"]) & admin_filter)
+@app.on_message(filters.command(["ucy","ucy"], prefixes=["l", "L"]) & admin_filter)
 async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
