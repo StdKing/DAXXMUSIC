849fe8e569b087bc460736f378df86a916059491
STD KING <112249837+StdKing@users.noreply.github.com>
2024-03-15 18:19:24 +0530

    Update welcome.py

--- DAXXMUSIC/plugins/tools/welcome.py
+++ DAXXMUSIC/plugins/tools/welcome.py
@@ -78,7 +78,7 @@ def circle(pfp, size=(200, 200), brightness_factor=10):
     return pfp
 
 def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
-    background = Image.open("DAXXMUSIC/assets/wel2.jpg")
+    background = Image.open("DAXXMUSIC/assets/wel2.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp, brightness_factor=brightness_factor) 
     pfp = pfp.resize((205, 205))
@@ -95,7 +95,7 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     #
     pfp_position = (48, 88)
     background.paste(pfp, pfp_position, pfp)
-    background.save(f"downloads/welcome#{id}.jpg")
+    background.save(f"downloads/welcome#{id}.png")
     return f"downloads/welcome#{id}.png"
 
 
@@ -149,7 +149,7 @@ async def greet_new_member(_, member: ChatMemberUpdated):
                 user.photo.big_file_id, file_name=f"pp{user.id}.png"
             )
         except AttributeError:
-            pic = "DAXXMUSIC/assets/wel2.jpg"
+            pic = "DAXXMUSIC/assets/wel2.png"
         if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
             try:
                 await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
